97d6be046391d6932735feb0a1684b9bbff9d561
Kedaar Vyas <114265496+kedaarvyas@users.noreply.github.com>
2023-10-07 15:57:09 -0400

    Update d6_skeleton.ml

--- discussions/d6_ocaml_hof/src/d6_skeleton.ml
+++ discussions/d6_ocaml_hof/src/d6_skeleton.ml
@@ -20,10 +20,14 @@ let rec fold_right f lst a =
     |h::t-> f h (fold_right f t a)
 
 (* TODO *)
-let rec fold_tree f b t = failwith "unimplemented"
+let rec fold_tree f b t = match t with
+Leaf -> b
+|Node(l, x, r) -> f (fold_tree f b l) x (fold_tree f b r)  
 
-let rec map_tree f t = failwith "unimplemented"
+let rec map_tree f t = match t with
+Leaf -> Leaf
+|Node(l,x,r) -> Node((map_tree f l), (f x), (map_tree f r))
 
-let add1 tree = failwith "unimplemented"
+let add1 tree = map_tree (fun x -> x + 1) tree
 
-let sum tree = failwith "unimplemented"
+let sum tree = fold_tree (fun l x r -> l + x + r) 0 tree
